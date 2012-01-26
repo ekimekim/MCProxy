0b932970c88c5e20a05e19afc7e31b7945ba102a
minecraft server <minecraft@49.156.19.217>
2012-01-26 13:18:49 +1100

    Minor config stuff

--- plugins.py
+++ plugins.py
@@ -3,7 +3,7 @@ from config import *
 sys.path.append(PLUGIN_PATH)
 
 # Import plugins here
-import log_all, log_sorted, username
+import log_all, log_sorted, usernames
 
 # Set ordering here
-plugins = [log_sorted, username]
+plugins = [log_all, log_sorted, usernames]
--- plugins/usernames.py
+++ plugins/usernames.py
@@ -3,6 +3,6 @@ def on_start():
 	pass
 
 def on_packet(packet, user, to_server):
-	if packet.name() == 'Handshake':
+	if to_server and packet.name() == 'Handshake':
 		user.username = packet.data['username']
 	return packet
