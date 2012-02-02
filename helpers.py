4d551592de6380b05d3b111f2d7338ee4c75904a
minecraft server <minecraft@49.156.19.217>
2012-02-02 22:48:52 +1100

    debugging

--- helpers.py
+++ helpers.py
@@ -1,8 +1,7 @@
+# -*- coding: cp1252 -*-
 """Helper functions for plugins"""
 
 import os
-
-
 from proxy import send_packet
 from proxy import server_cmd as cmd
 from config import *
@@ -12,3 +11,27 @@ def ops():
 	ret = open(os.path.join(SERVER_DIR, 'ops.txt')).read().strip().split('\n')
 	ret = [unicode(name) for name in ret]
 	return ret
+
+def color(name):
+	"""Takes a color name and returns the string needed to turn the chat message that color."""
+	color_map = {
+		'black': u'0',
+		'dark blue': u'1',
+		'dark green': u'2',
+		'dark cyan': u'3',
+		'dark red': u'4',
+		'purple': u'5',
+		'gold': u'6',
+		'gray': u'7',
+		'dark gray': u'8',
+		'blue': u'9',
+		'green': u'a',
+		'cyan': u'b',
+		'red': u'c',
+		'pink': u'd',
+		'yellow': u'e',
+		'white': u'f'
+	}
+	if name not in color_map:
+		raise ValueError('Bad color %s' % repr(name))
+	return u'§' + color_map[name]
--- plugins/sp_opcolor.py
+++ plugins/sp_opcolor.py
@@ -1,4 +1,3 @@
-# -*- coding: cp1252 -*-
 
 AUTHOR='sleepyparadox'
 CONTACT='sleepyparadox@gmail.com'
@@ -8,8 +7,7 @@ Highlights 'Sleepy Paradox' and ekimekim' in red.
 Also replaces 'bitblitz' with 'Sleepy Paradox' (To make account consistent with developer name)"""
 
 from packet_decoder import Packet
-from helpers import send_packet
-from helpers import ops
+from helpers import ops, color
 
 opColor = color('red')
 
@@ -28,27 +26,3 @@ def on_packet(packet, user, to_server):
             packet.data['text'] = packet.data['text'].replace(u'bitblitz', u'Sleepy Paradox')
     #return edited packet
     return packet
-
-def color(name):
-	"""Takes a color name and returns the string needed to turn the chat message that color."""
-	color_map = {
-		'black': u'0',
-		'dark blue': u'1',
-		'dark green': u'2',
-		'dark cyan': u'3',
-		'dark red': u'4',
-		'purple': u'5',
-		'gold': u'6',
-		'gray': u'7',
-		'dark gray': u'8',
-		'blue': u'9',
-		'green': u'a',
-		'cyan': u'b',
-		'red': u'c',
-		'pink': u'd',
-		'yellow': u'e',
-		'white': u'f'
-	}
-	if color not in color_map:
-		raise ValueError('Bad color')
-	return u'§' + color_map[color]
