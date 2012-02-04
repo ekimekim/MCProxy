cd08a19d9cf39a8c296c5b426a824a8888066d10
Mike Lang <MikeLang3000@gmail.com>
2012-02-05 00:59:22 +1100

    Added cmd infratructure, tell infrastructure. Needs some extra stuff before it can work.

--- helpers.py
+++ helpers.py
@@ -4,6 +4,7 @@
 import os
 from proxy import send_packet
 from proxy import server_cmd as cmd
+from packet_decoder import names as packet_names
 from config import *
 
 def ops():
@@ -12,9 +13,7 @@ def ops():
 	ret = [unicode(name) for name in ret]
 	return ret
 
-def color(name):
-	"""Takes a color name and returns the string needed to turn the chat message that color."""
-	color_map = {
+colors = {
 		'black': u'0',
 		'dark blue': u'1',
 		'dark green': u'2',
@@ -32,6 +31,37 @@ def color(name):
 		'yellow': u'e',
 		'white': u'f'
 	}
+def color(name):
+	"""Takes a color name and returns the string needed to turn the chat message that color."""
 	if name not in color_map:
 		raise ValueError('Bad color %s' % repr(name))
 	return u'§' + color_map[name]
+
+
+locks = set()
+def tell(user, message, delay=0, lock=None):
+	"""Send a server message to user.
+	Optional args:
+		delay: Wait delay seconds before sending message. Useful mainly with lock. See below.
+		lock: Until message sent (see delay) allow no new messages with the same user and lock value.
+			Generally speaking, expected to be a string. But it doesn't really matter.
+	Returns bool of whether message was sent (see lock)
+	"""
+	global locks
+	if lock is not None:
+		if (user, lock) in locks:
+			return False
+		else:
+			locks.add((user, lock))
+	def tell_send():
+		packet = Packet()
+		packet.ident = packet_names['Chat message']
+		packet.direction = CLIENT_TO_SERVER if to_server else SERVER_TO_CLIENT
+		packet.data = {'text': unicode(message)}
+		send_packet(packet, user, False)
+		locks.remove((user, lock))
+
+	if delay:
+		schedule(tell_send, delay)
+	else:
+		tell_send()
