87baff27bdb9dcbb818e8fc696df96c9c2c65e2b
Mike Lang <MikeLang3000@gmail.com>
2012-03-06 14:32:56 +1100

    Progress on rewrite

--- plugins/sp_zone.py
+++ plugins/sp_zone.py
@@ -1,18 +1,29 @@
 from helpers import point_dist
+from persistent_store import Store
 
-# Format for a zone
-# zone := (name, acls, tags, bounds_info)
-# acls := {username:[acl_record], ...}
-#	Special username "<everyone>" applies to any unlisted username
-# acl_record := subset of ['enter', 'interact', 'modify', 'creative', 'admin'] - meanings:
-#	'enter': Player may walk into zone
-#	'interact': Player may interact with objects (levers, buttons, etc)
-#	'modify': Player may place/destroy blocks
-#	'creative': On creative server, player may use creative mode
-#	'admin': Player may modify zone properties
-# bounds_info := (style, *args)
-#	style is str
+new_zone_hooks = [] # Append to this list to register a function to call for new zones
+# Function should take args (zone)
 
+# bounds_info is a member of Zone, and encodes info on the bounds of the zone.
+# Is a tuple (style, *args). Style is one of those listed in bounds_styles below.
+# Args is dependent on style.
+
+# Notes
+#	Zones are dicts
+#	If two zones overlap, you need permissions in BOTH to do the requested action.
+#	For now, only priveliged users can make zones (potential for abuse)
+#		Consider revising - propose and accept system?
+
+def new_zone(name, bounds_info, creator):
+	zone = {}
+	zone['name'] = name
+	zone['creator'] = creator
+	zone['bounds_info'] = bounds_info
+	for fn in new_zone_hooks:
+		fn(zone)
+	return zone
+
+# --- bounds code ---
 
 def bounds_cube(dim, a, b):
 	ranges = zip(a,b) # Ranges is a list of (a_coord, b_coord)
@@ -34,35 +45,29 @@ def bounds_cyl(dim, base, radius, height)
 
 def bounds_union(*bounds_infos):
 	def union_test(current_dim, point):
-		return any(check_bounds(*bounds_info)(current_dim, point) for bounds_info in bounds_infos)
+		return any(get_bounds_fn(*bounds_info)(current_dim, point) for bounds_info in bounds_infos)
+
 
 bounds_styles = {'cube': bounds_cube, 'cylinder': bounds_cyl}
 
-def check_bounds(style, *args):
+def get_bounds_fn(style, *args):
 	fn = bounds_styles[style](*args)
 	return fn
 
+# --- End bounds code ---
 
 def on_start():
-	global aZones
-	aZones = []
-	aZones.append( CreateZoneCube("Wolf's Run", Vec3(92, 0, -53), Vec3(-43, 256, -216)))
-	#aZones[0].aTags = [u"City"]
-	aZones.append( CreateZoneCube("Pants", Vec3(100,0,100), Vec3(-100,256,-100)))
-	#aZones[1].aTags = [u"NoBuild"]
-	SaveZones()
-	
-	
+	global zones = Store('zones') # This also loads up any old zones
+
+
 def on_packet(packet, user, to_server):
-	global aZones
+	global zones
 
 	#if dim change
 	if packet.name() == "Login request" and not to_server:
-		user.dimension = GetDimensionName(packet.data["dimension"])
-		print user.dimension
+		user.dimension = packet.data["dimension"]
 	if packet.name() == "Respawn":
-		user.dimension = GetDimensionName(packet.data["dimension"])
-		print user.dimension
+		user.dimension = packet.data["dimension"]
 		
 	#if movement
 	if packet.name() == 'Player position' or packet.name() == 'Player position & look':
