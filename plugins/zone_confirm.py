ef152f48052795fb3482702ea8e68899ed923f7f
Mike Lang <MikeLang3000@gmail.com>
2012-04-21 07:56:26 +1000

    Added seperate plugin for zone confirming. TODO actually do acl_cmds TODO finish the above TODO zone_cmds need to allow ops to use them for other ppls zones.

---
+++ plugins/zone_confirm.py
@@ -0,0 +1,53 @@
+
+AUTHOR = 'ekimekim'
+CONTACT = 'mikelang3000@gmail.com'
+DESCRIPTION = """Newly created zones must be submitted for op approval.
+Ops gain commands:
+	/zone op help
+	/zone op wool ZONE - Paint 10x10x10 area about and below op in green woll for requested zone, red wool other zone, yellow if both.
+	/zone op confirm ZONE
+"""
+
+import zones
+from player_cmd import register
+from helpers import ops
+
+def set_confirm(zone):
+	zone['confirmed'] = False
+
+def on_start():
+	zones.new_zone_hooks.append(set_confirm)
+	register("/zone op help", ophelp)
+	register("/zone op wool (.*)", woolzone)
+	register("/zone op confirm (.*)", confirmzone)
+
+def ops_only(fn):
+	def wrapped_fn(message, user, *args):
+		if user not in ops():
+			return False
+		else:
+			return fn(message, user, *args)
+
+@ops_only
+def ophelp(message, user):
+	tell(user, "Note that unlike /zone commands, zone names are taken verbatim (no quotes).\n"
+	           "/zone op help - Obvious.\n"
+	           "/zone op wool <zone> - 10x10x10 cube below player highlighted as follows:\n"
+	           "__ green: In given zone.\n"
+	           "__ red: In zone besides one given.\n"
+	           "__ yellow: In given zone AND at least one other.\n"
+	           "__ You will need to log out/in to reverse changes.\n"
+	           "/zone op confirm <zone> - Confirm zone."
+
+
+@ops_only
+def zoneconfirm(message, user, name):
+	if name not in zones.get_zones():
+		tell(user, "Zone does not exist.")
+		return
+	zones.get_zones()['confirmed'] = True
+
+
+@ops_only
+def zonewool(message, user, name):
+	pass # TODO
