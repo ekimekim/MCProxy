451e612a6d03377493024603ac1b27d6d9e2ce47
minecraft server <minecraft@49.156.19.217>
2012-04-20 17:08:49 +1000

    acl commands. not done yet.

---
+++ plugins/acl_cmds.py
@@ -0,0 +1,10 @@
+
+AUTHOR = "ekimekim"
+CONTACT = "mikelang3000@gmail.com"
+DESCRIPTION = """Commands for controlling the acls of a zone:
+	/acls help
+	/acls set USER {PERMS}
+	/acls add USER PERM
+	/acls remove USER PERM
+	/acls clear USER
+"""
